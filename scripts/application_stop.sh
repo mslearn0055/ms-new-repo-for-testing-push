9b25c58fe53cf9a6a9db24b21b0bfd60ce4831bb
mslearn0055 <137442343+mslearn0055@users.noreply.github.com>
2024-10-01 21:33:38 +0530

    Update application_stop.sh

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
-#Stopping existing node servers
-#echo "Stopping any existing node servers"
-#sudo pkill node
+#!/bin/bash
+
+# application_start.sh
+
+# Remove existing package-lock.json if it exists
+if [ -f "/home/ubuntu/express-app/package-lock.json" ]; then
+    echo "Removing existing package-lock.json"
+    rm /home/ubuntu/express-app/package-lock.json
+fi
+
+# Start your application
+echo "Starting the application..."
+npm start
+
+Stopping existing node servers
+echo "Stopping any existing node servers"
+sudo pkill node
