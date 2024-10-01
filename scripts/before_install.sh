153e08627ce74e72ece40d627c67cc52dd508cc4
mslearn0055 <137442343+mslearn0055@users.noreply.github.com>
2024-10-01 21:46:29 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ -f "/home/ubuntu/express-app/package-lock.json" ]; then
+  echo "Removing existing package-lock.json"
+  rm /home/ubuntu/express-app/package-lock.json
+fi
 #download node and npm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
